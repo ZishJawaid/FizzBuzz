363ad845b167a8b8b7b0f6392dbcbcfdbb963c0d
JRG <47917431+JR-G@users.noreply.github.com>
2020-07-29 14:07:41 +0100

    Refactored to be a private class method

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,18 +2,18 @@ require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'Fizz'
+    expect(3.fizzbuzz).to eq 'Fizz'
   end
 
   it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq "Buzz"
+    expect(5.fizzbuzz).to eq "Buzz"
   end
 
   it 'returns "FizzBuzz" when passed a multiple of 3 and 5' do
-    expect(fizzbuzz(15)).to eq "FizzBuzz"
+    expect(15.fizzbuzz).to eq "FizzBuzz"
   end
 
   it 'returns the number if not multiple of 3 or 5' do
-    expect(fizzbuzz(2)).to eq 2
+    expect(2.fizzbuzz).to eq 2
   end
 end
